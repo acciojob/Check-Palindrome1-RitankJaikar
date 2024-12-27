a9624a80eb4d348a900c514bec5c6aa7627b004b
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-27 08:31:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// complete the given function
-
-function palindrome(str){
-
+function palindrome(str) {
+  // Normalize the string: remove non-alphanumeric characters and convert to lowercase
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+  // Check if the cleaned string is the same as its reverse
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  
+  return cleanedStr === reversedStr;
 }
-module.exports = palindrome
+
+module.exports = palindrome;
